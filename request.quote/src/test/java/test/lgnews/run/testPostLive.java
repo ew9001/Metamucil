5a4b521f1456502a649a6902c76bf07ed2d237c1
Earl Wilis <earl.willis@publicismodem.com>
2014-10-07 17:09:45 -0400

    Razr

--- request.quote/src/test/java/test/lgnews/run/testPostLive.java
+++ request.quote/src/test/java/test/lgnews/run/testPostLive.java
@@ -506,6 +506,7 @@ public class testPostLive {
 		 caps.setCapability("platform", "ANDROID");
 		 caps.setCapability("device", "Motorola Razr Maxx HD");
 		 caps.setCapability("browserstack.debug", "true");
+		 caps.setCapability("browserstack.local", "true");
 		    driver = new RemoteWebDriver(new URL(URL), caps);
 	      System.out.println("Let me run mobileRazorHD");
 	     
